998fcdd352edfda9ca5ac101b61ba5de8e6c3e7b
goppd <goppdima@gmail.com>
2025-11-23 17:03:28 +0100

    Added variables and functions to  project

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import './App.css'
+import React from 'react'
+import ToggleText from './components/ToggleText'
 
 function App() {
-  return <></>
+  return (
+    <div>
+      <ToggleText />
+    </div>
+  )
 }
 
 export default App
--- src/components/ToggleText/index.jsx
+++ src/components/ToggleText/index.jsx
@@ -1,4 +1,38 @@
-import React, { useRef, useEffect } from 'react'
-import styles from '../module.css'
+import React, { useRef, useState } from 'react'
+import styles from './styles.module.css'
 
-function ToggleText() {}
+function ToggleText() {
+  const textRef = useRef(null)
+  const [isVisible, setIsVisible] = useState(false)
+  const [duration, setDuration] = useState(0.5)
+
+  const toggleText = () => {
+    if (!textRef.current) return
+
+    if (isVisible) {
+      textRef.current.style.maxHeight = '0px'
+      textRef.current.style.opacity = 0
+    } else {
+      textRef.current.style.maxHeight = textRef.current.scrollHeight + 'px'
+      textRef.current.style.opacity = 1
+    }
+
+    setIsVisible(!isVisible)
+  }
+
+  const handleDurationChange = (e) => {
+    const value = parseFloat(e.target.value)
+    if (!isNaN(value) && value >= 0) {
+      setDuration(value)
+      if (textRef.current) {
+        textRef.current.style.transition = `all ${value}s ease`
+      }
+    }
+  }
+
+  return (
+    
+  )
+}
+
+export default ToggleText
